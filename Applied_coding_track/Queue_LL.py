d7f39a9bc4c8e2324d8fa9e51b0660915ca39663
Ramanand Gupta <ramanand.gupta.2244@gmail.com>
2022-01-02 18:34:33 +0530

    Queue LL python code

---
+++ Applied_coding_track/Queue_LL.py
@@ -0,0 +1,61 @@
+# code for the Queue linked list in Python
+
+
+class Node:
+    def __init__(self, info, next = None):
+        self.info = info
+        self.next = next
+
+# Enqueue class
+class Queue:
+    def __init__(self):
+        self.head = self.tail = None
+
+    def EnQueue(self,ele):
+        temp = Node(ele)
+        if self.tail == None:
+            self.head = self.tail = temp
+            return
+        self.tail.next = temp
+        self.tail = temp
+
+    def isEmpty(self):
+        return self.head == None
+
+    def DeQueue(self):
+        if self.isEmpty():
+            print("The Queue is Empty\n")
+            return
+        else:
+            temp = self.head
+            self.head = temp.next
+            if self.head == None:
+                self.tail = None
+
+
+    def Display(self):
+        if self.isEmpty():
+            print("nothing to print the Queue is empty\n")
+            return
+        temp = self.head
+        print("The value into the Queue are -: ")
+        while( temp != None):
+            print(temp.info, end = " -> ")
+            temp = temp.next
+        print("\n")
+
+
+q = Queue()
+q.EnQueue(25)
+q.EnQueue(17)
+q.EnQueue(45)
+q.EnQueue(67)
+q.EnQueue(59)
+q.EnQueue(100)
+q.Display()
+print(q.head.info)
+q.DeQueue()
+print(q.head.info)
+q.Display()
+
+
